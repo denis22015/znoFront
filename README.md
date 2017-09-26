c893f082f688713cd583722ac0ba76b3f14e3c05
denis <denis@localhost.localdomain>
2017-09-26 19:52:57 +0300

    created Hello react with webpack :3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# znoFront
+# redux-ru-tutorial
+Код для раздела [Подготовка/ESLint](https://maxfarseer.gitbooks.io/redux-course-ru/content/eslint.html)
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,52 @@
-webpack = require('webpack');
-path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
-
-webpackConfig = {
-    context: __dirname,
-    entry: {
-        bundle: './static/app.js',
-        styles: './static/main.scss'
-    },
-    output: {
-        filename: '[name].js',
-        path: './static/build',
-        library: '[name]'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devtool: '#cheap-module-source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: [/node_modules/],
-                loader: "babel-loader",
-                query: {
-                    presets: ['es2015', 'react', 'stage-0', 'stage-1']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            },
-            {
-                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css', {
-            allChunks: true
-        })
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new NpmInstallPlugin()
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
+    loaders: [
+      {
+        loaders: ['react-hot', 'babel-loader'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        test: /\.js$/,
+        plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+      }
     ]
-};
-module.exports = webpackConfig;
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
+  }
+}
