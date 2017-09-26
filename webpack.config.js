5fb10fff80f1ee987286f8cfb722982252ee5ef9
denis <denis@localhost.localdomain>
2017-09-26 18:54:42 +0300

    new React Webpack tutorial

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+webpack = require('webpack');
+path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+webpackConfig = {
+    context: __dirname,
+    entry: {
+        bundle: './static/app.js',
+        styles: './static/main.scss'
+    },
+    output: {
+        filename: '[name].js',
+        path: './static/build',
+        library: '[name]'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: '#cheap-module-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: [/node_modules/],
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react', 'stage-0', 'stage-1']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
+    ]
+};
+module.exports = webpackConfig;
