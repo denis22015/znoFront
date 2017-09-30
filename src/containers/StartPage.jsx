fc29b7bb99eb69d94f79bc74bf72aec12b466df5
denis <denis@localhost.localdomain>
2017-09-30 18:58:50 +0300

    startPage imports

---
+++ src/containers/StartPage.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+
+//import Components
+import About from '../components/StartPage/About.jsx'
+import Contact from '../components/StartPage/Contact.jsx'
+import Download from '../components/StartPage/Download.jsx'
+import Footer from '../components/StartPage/Footer.jsx'
+import Intro from '../components/StartPage/Intro.jsx'
+import Header from '../components/StartPage/Header.jsx'
+import Map from '../components/StartPage/Map.jsx'
+
+
+
+//impost styles
+import '../styles/bootstrap/css/bootstrap.min.css'
+import './../styles/font-awesome/css/font-awesome.min.css'
+import './../styles/lora.css'
+import './../styles/cabin.css'
+import './../styles/grayscale.css'
+ // import 'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic'
+ // import 'https://fonts.googleapis.com/css?family=Cabin:700'
+
+
+
+
+
+
+export default class StartPage extends Component {
+  render() {
+    return(
+		<div>
+			<Header />
+			<Intro />
+			<About />
+			<Download />
+			<Contact />
+			<Map />
+			<Footer />
+		</div>
+     )
+  }
+}
+
+
+
+//import js files
+require('../js/jquery.min.js');
+// import '../js/jquery.min.js'
+// import '../js/popper.min.js'
+// import '../js/bootstrap.min.js'
+// import '../js/jquery.easing.min.js'
+//import '../js/grayscale.min.js'
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: [
     'webpack-hot-middleware/client',
     'babel-polyfill',
-    './src/index'
+    './src/index.jsx'
   ],
   output: {
     path: path.join(__dirname, 'static'),
@@ -20,36 +20,56 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new NpmInstallPlugin()
+
+
   ],
   module: {
     preLoaders: [
-      {
-        test: /\.js$/,
-        loaders: ['eslint'],
-        include: [
-          path.resolve(__dirname, "src"),
-        ],
-      }
+      // {
+      //   test: /\.js$/,
+      //   loaders: ['eslint'],
+      //   include: [
+      //     path.resolve(__dirname, "src"),
+      //   ],
+      // }
     ],
     loaders: [
+      // {
+      //   loaders: ['react-hot', 'babel-loader'],
+      //   include: [
+      //     path.resolve(__dirname, "src"),
+      //   ],
+      //   test: /\.js$/,
+      //   plugins: ['transform-runtime'],
+      // },
       {
-        loaders: ['react-hot', 'babel-loader'],
-        include: [
-          path.resolve(__dirname, "src"),
-        ],
-        test: /\.js$/,
-        plugins: ['transform-runtime'],
+        test: /\.exec\.js$/,
+        loader: [ 'script-loader' ]
       },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: 'css?sourceMap!less?sourceMap'
+      }, 
       {
         test:   /\.css$/,
         loader: "style-loader!css-loader!postcss-loader!"
-      },  { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      }, 
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
 
       {
-      test: /\.(png|jpg)$/,
-      loader: 'url?limit=25000'
-    }
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=25000'
+      }
+
 
     ]
   },
@@ -57,3 +77,4 @@ module.exports = {
     return [autoprefixer, precss];
   }
 }
+
